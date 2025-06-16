74895c063185383744d0e27b11cd2489332c3fec
lou437104 <louiseguest73@gmail.com>
2025-06-16 08:33:04 +0100

    🎯 Bullseye – it works!

---
+++ game.js
@@ -0,0 +1,15 @@
+const board = document.getElementById("game-board");
+const message = document.getElementById("message");
+
+// ive chose logos that i think are relevant for the charity ball the autisic society because we work within the sen department
+
+const logos =[
+    "images/british-heart-foundation-logo-1000x1000px.jpg",
+   "images/Cancer-Research-UK-Logo-2012.png",
+   "images/Macmillan-Cancer-Support-Symbol.png",
+   "images/Marie_Curie_Logo_2023_Landscape_CMYK-pdf.jpg",
+   "images/Mind-Logo.png",
+   "images/nas-1-1024x512.png",
+   "images/St-Giles-logo_Hospice_orange-7-800x422.png",
+   "images/Teenage-Cancer-Trust_social-share_1.png"
+]
