8311378f3ac0936a257afa4cad9ef456d92bfbba
lou437104 <louiseguest73@gmail.com>
2025-06-16 08:47:22 +0100

    🎯 Bullseye – it works!

--- game.js
+++ game.js
@@ -11,5 +11,13 @@ const logos =[
    "images/Mind-Logo.png",
    "images/nas-1-1024x512.png",
    "images/St-Giles-logo_Hospice_orange-7-800x422.png",
+   "images/Teenage-Cancer-Trust_social-share_1.png",
+   "images/british-heart-foundation-logo-1000x1000px.jpg",
+   "images/Cancer-Research-UK-Logo-2012.png",
+   "images/Macmillan-Cancer-Support-Symbol.png",
+   "images/Marie_Curie_Logo_2023_Landscape_CMYK-pdf.jpg",
+   "images/Mind-Logo.png",
+   "images/nas-1-1024x512.png",
+   "images/St-Giles-logo_Hospice_orange-7-800x422.png",
    "images/Teenage-Cancer-Trust_social-share_1.png"
 ]
