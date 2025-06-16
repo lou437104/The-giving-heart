6f32a05dfad65b6afa4ce0627c83853aca6a1a95
lou437104 <louiseguest73@gmail.com>
2025-06-16 18:18:57 +0100

    🎯 Bullseye – it works!

--- game.js
+++ game.js
@@ -21,3 +21,27 @@ const logos =[
    "images/St-Giles-logo_Hospice_orange-7-800x422.png",
    "images/Teenage-Cancer-Trust_social-share_1.png"
 ]
+
+let cards = {...logos, ...logos}; //create 8 pairs of cards
+cards = cards.sort(() => 0.5 - Math.random()); //shuffle the cards
+
+let flippedCards = [];
+let matchedCount = 0;
+function createCard(imageSrc) {
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    const front = document.createElement("div");
+    front.classList.add("front");
+
+    const back = document.createElement("div");
+    back.classList.add("back");
+
+    const img = document.createElement("img");
+    img.src = imageSrc;
+    back.appendChild(img);
+
+    card.appendChild(front);
+    card.appendChild(back);
+    
+}
